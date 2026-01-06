787b4526c0a2aa2c4876bd37fa2e8bf180e3ab34
Yuk1ia <Yankai.Zhao21@student.xjtlu.edu.cn>
2026-01-06 20:12:01 +0800

    first commit: spring-boot initializr

---
+++ src/test/java/com/github/Yuk1ia/MyWebPage/MyWebPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Yuk1ia.MyWebPage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
